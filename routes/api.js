48d3312d2d2b34d6d0ccc3bca693aec00317e6e5
jaymark <jaykmark@gmail.com>
2020-01-09 14:19:47 -0800

    refactor to RESTful routing

--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,24 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const ctrl = require("../controllers");
+const ctrl = require('../controllers');
 
 //CITIES
-router.get("/cities", ctrl.cities.index);
-router.post("/cities/create", ctrl.cities.create);
-router.get("/cities/:cityId", ctrl.cities.show);
-router.put("/cities/update/:cityId", ctrl.cities.update);
-router.delete("/cities/delete/:cityId", ctrl.cities.destroy);
+router.get('/cities', ctrl.cities.index);
+router.post('/cities', ctrl.cities.create);
+router.get('/cities/:id', ctrl.cities.show);
+router.put('/cities/update/:id', ctrl.cities.update);
+router.delete('/cities/delete/:id', ctrl.cities.destroy);
 
 // POSTS
-router.get("/posts", ctrl.posts.allPosts);
-router.post("/posts/newpost", ctrl.posts.newPost);
-router.get("/posts/:postId", ctrl.posts.postDetail);
-router.put("/posts/:postId", ctrl.posts.editPost);
-router.delete("/posts/deletePost/:postId", ctrl.posts.deletePost);
+router.get('/posts', ctrl.posts.allPosts);
+router.post('/posts', ctrl.posts.newPost);
+router.get('/posts/:id', ctrl.posts.postDetail);
+router.put('/posts/:id', ctrl.posts.editPost);
+router.delete('/posts/:id', ctrl.posts.deletePost);
 
 // COMMENTS
-router.get("/comments", ctrl.comments.index);
-router.post('/comments/create', ctrl.comments.create);
+router.get('/comments', ctrl.comments.index);
+router.post('/comments', ctrl.comments.create);
 router.get('/comments/:id', ctrl.comments.show);
 router.put('/comments/:id', ctrl.comments.upDate);
 router.delete('/comments/:id', ctrl.comments.destroy);
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const ctrl = require("../controllers");
+const ctrl = require('../controllers');
 
-router.post("/register", ctrl.auth.register);
-router.post("/login", ctrl.auth.login);
-router.get("/verify", ctrl.auth.verify);
-router.delete("/logout", ctrl.auth.logout);
-router.put("/update/:userId", ctrl.auth.updateUser);
-router.get("/:userId", ctrl.auth.userDetail);
-router.delete("/delete/:userId", ctrl.auth.deleteUser);
+router.post('/register', ctrl.auth.register);
+router.post('/login', ctrl.auth.login);
+router.get('/verify', ctrl.auth.verify);
+router.delete('/logout', ctrl.auth.logout);
+router.put('/update/:id', ctrl.auth.updateUser);
+router.get('/:id', ctrl.auth.userDetail);
+router.delete('/delete/:id', ctrl.auth.deleteUser);
 
 module.exports = router;
